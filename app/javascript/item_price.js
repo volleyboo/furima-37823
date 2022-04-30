5fed55348f659245d3d72850a53ddecc9fe95e6f
volleyboo <volleyboo.0821@gmail.com>
2022-04-30 15:46:49 +0900

    JavaScript実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,5 +1,17 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
+  
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue*0.1)
+
+    const profitDom = document.getElementById("profit");
+    profitDom.innerHTML = Math.floor(inputValue*0.9)
+
+    console.log(inputValue);
+ })
   console.log(priceInput);
   console.log("OK");
 });
