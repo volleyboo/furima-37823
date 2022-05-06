497497d6a45396ec9989a2ba4fb3714adfb4186e
volleyboo <volleyboo.0821@gmail.com>
2022-05-07 08:16:47 +0900

    Formオブジェクト単体テスト（途中）

--- spec/factories/purchase_forms.rb
+++ spec/factories/purchase_forms.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :purchase_form do
-    
+    postal_code { '123-4567' }
+    area_id { 2 }
+    municipality { '東京都' }
+    address { '1-1' }
+    building { '柳ビル' }
+    telephone_number { 11111111111 }
   end
 end
--- spec/models/purchase_form_spec.rb
+++ spec/models/purchase_form_spec.rb
@@ -1,5 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseForm, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @purchase_form = FactoryBot.build(:purchase_form)
+  end
+
+  describe '配送先情報情報の保存' do
+    context '保存ができる場合' do
+      it '全て存在すれば保存できる' do
+        expect(@purchase_form).to be_valid
+      end
+      it 'buildingは空でも保存できる' do
+        @purchase_form.building = ''
+        expect(@purchase_form).to be_valid
+      end
+    end
+
+    context '保存ができない場合' do
+      it 'postal_code:必須' do
+        @purchase_form.postal_code = ''
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できない' do
+        @purchase_form.postal_code = '1234567'
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include('Postal code is invalid. Enter it as follows (e.g. 123-4567)')
+      end
+      it 'area:必須' do
+        @purchase_form.area_id = ''
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Area can't be blank")
+      end
+      it 'area:2以上のidが選択されないと保存できない' do
+        @purchase_form.area_id = '1'
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Area can't be blank")
+      end
+      it 'municipality:必須' do
+        @purchase_form.municipality = ''
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Municipality can't be blank")
+      end
+      it 'address:必須' do
+        @purchase_form.address = ''
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Address can't be blank")
+      end
+      it 'telephone_number:必須' do
+        @purchase_form.telephone_number = ''
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Telephone number can't be blank")
+      end
+      it '電話番号は半角数値のみ保存できる' do
+        @purchase_form.telephone_number = '１234'
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Telephone number is invalid. Input only number")
+      end
+      it '電話番号は9桁以下だと保存できない' do
+        @purchase_form.price = '111111111'
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Telephone number is too short")
+      end
+      
+    end
+  end
 end
